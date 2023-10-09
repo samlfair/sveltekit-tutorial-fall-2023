2a86c63e1d8a9fe4ec9e385800cc21f8e476784d
Sam Littlefair <sam.littlefair@prismic.io>
2023-10-09 20:05:27 +0200

    set global styles

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -7,19 +7,14 @@
 </script>
 
 <section class="container">
-	<PrismicRichText
-		field={slice.primary.content}
-		components={{
-			label: Label
-		}}
-	/>
+	<div class="text">
+		<PrismicRichText field={slice.primary.content} components={{ label: Label }} />
+	</div>
 </section>
 
 <style>
-	.container {
-		max-width: 600px;
-		margin: 6em auto;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+	.text {
+		width: 100%;
+		max-width: var(--size-content-3);
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,3 +18,72 @@
 <main>
 	<slot />
 </main>
+
+<style>
+	/* Import Open Props styles */
+	@import 'open-props/style';
+	@import 'open-props/normalize';
+	@import 'open-props/gray-hsl';
+
+	/* Declare a font */
+	@font-face {
+		font-family: 'Anton';
+		font-style: normal;
+		font-weight: 100 900;
+		font-display: swap;
+		src: local(''), url('/Anton.woff2') format('woff2');
+		unicode-range: U+20-7E, U+E4, U+E5, U+F6;
+	}
+
+	/* Define colors scheme for light and dark mode */
+	:root {
+		--brand: var(--red-6);
+		--text-1: var(--gray-9);
+		--text-2: var(--gray-11);
+		--surface-1: var(--gray-3);
+		--surface-2: var(--indigo-2);
+		--surface-3: var(--gray-2);
+		--surface-4: var(--orange-4);
+		--surface-shadow: var(--gray-8-hsl);
+		--shadow-strength: 2%;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:root {
+			color-scheme: dark;
+
+			--brand: var(--orange-10);
+			--text-1: var(--gray-3);
+			--text-2: var(--gray-2);
+			--surface-1: var(--gray-9);
+			--surface-2: var(--gray-10);
+			--surface-3: var(--purple-12);
+			--surface-4: var(--violet-11);
+			--surface-shadow: var(--gray-11-hsl);
+			--shadow-strength: 50%;
+		}
+	}
+
+	/* Set fonts */
+	:global(html) {
+		--font-serif: 'Anton';
+		font-family: var(--font-sans);
+	}
+
+	/* Add some global styling for website sections */
+	:global(section) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--size-10);
+		padding: var(--size-11) 0;
+	}
+
+	:global(section:nth-child(even)) {
+		background: var(--surface-1);
+	}
+
+	:global(section:nth-child(odd)) {
+		background: var(--surface-4);
+	}
+</style>
